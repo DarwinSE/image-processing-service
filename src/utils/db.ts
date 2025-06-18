54ab6c1ebdceb1ccc02e0d55fcc044e200a0eccd
Darwin Guerra <darwin.guerra@seneca-corp.com>
2025-06-18 10:12:12 -0500

    configuration of utils db connection

---
+++ src/utils/db.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+import { config } from 'dotenv'
+
+config()
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI as string)
+        console.log('Connected to MongoDB')
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error)
+    }
+}
+
+export default connectDB
